109320c7d135de9932df060bd87f686b031b5074
SoumyaMajhi <79470209+SoumyaMajhi@users.noreply.github.com>
2023-04-24 04:33:44 +0530

    Update samyfoundry.py
    added feature of converting from html to pdf instead of the earlier txt format

--- samyfoundry.py
+++ samyfoundry.py
@@ -1,8 +1,20 @@
 #pip install cfscrape  #installing Cloudflare-scrape to bypass Cloudflare security and captcha
+
+'''
+#---------FOR GOOGLE COLAB TO INSTALL PDFKIT AND WKHTMLTOPDF-------
+
+#!cat /etc/os-release
+#!uname -m
+!pip install pdfkit
+!wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.bionic_amd64.deb
+!cp wkhtmltox_0.12.6-1.bionic_amd64.deb /usr/bin
+!sudo apt install /usr/bin/wkhtmltox_0.12.6-1.bionic_amd64.deb
+'''
+
 #0. Set up requirements
 import cfscrape
-import requests
 from bs4 import BeautifulSoup
+import pdfkit
 
 url=input('Enter Sanf*undry page URL containing all Topic links of a Chapter: ')
 #1. Get the HTML
@@ -19,7 +31,9 @@ divs=soup.find('div', class_='entry-content')
 lists=divs.find_all('table')
 
 filename=url.split('/')[3]
-f=open( filename + '.txt','ab')
+f=open( filename + '.html','ab')  #create html file for storing the scraped data
+f.write('<!DOCTYPE html> \n <html>\n <head> \n <meta charset="UTF-8"> \n </head> \n <body>'.encode())
+f.seek(0)
 
 topics_total=0
 qna_total=0
@@ -29,7 +43,7 @@ for l in lists:
         topics_total+=1
         links=t['href']
         topicnames=t.text
-        f=open( filename + '.txt','ab')
+        f=open( filename + '.html','ab')
         f.write("\n\n\t\t".encode()+"Topic ".encode()+str(topics_total).encode()+". ".encode()+topicnames.encode()+'\n\n'.encode())
         f.seek(0)
 
@@ -70,7 +84,7 @@ for l in lists:
                 # Remove empty tag
                 child.decompose()
         
-        #removing extra divs with no class (specially sanfoundry ad links)
+        #removing extra divs with no class (specially sanf*undry ad links)
         for child in divs.find_all("div", class_=False):
             child.decompose()
 
@@ -84,8 +98,9 @@ for l in lists:
             child.decompose()
         
         #questions
-        questions=divs.find_all('p')[1:]   #no need, was needed for some earlier version of the code
+        questions=divs.find_all('p')[1:]
         #print(questions)
+
         #answers
         ans=divs.findAll('div',class_='collapseomatic_content')
 
@@ -97,13 +112,29 @@ for l in lists:
             if child in ans:
                 qna_total+=1
             if child == ans[-1]:
-                f.write(child.text.encode())
+                f.write(str(child).encode())
                 f.seek(0)
                 break
             else:
-                f.write(child.text.encode())
+                f.write(str(child).encode())
                 f.seek(0)
+
+f.write('\n\n\t\t'.encode() + 'Total Topics Found: '.encode() + str(topics_total).encode())
+f.seek(0)
+f.write('\n\t\t'.encode() + 'Total Q/A Found: '.encode() + str(qna_total).encode())
+f.seek(0)
+
+f.write('</body> \n </html>'.encode())
+f.seek(0)
+
+f.close()
+
+#convert html to pdf
+pdfkit.from_file( filename + '.html', filename + '.pdf')
+
 print("End")
+print('Total Topics Found: ',topics_total)
+print('Total Q/A Found: ',qna_total)
       
 f.close()
 print('Total Topics Found: ',topics_total)
